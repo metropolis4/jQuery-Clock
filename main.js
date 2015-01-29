6e0c228eda932faa68d7c1658795474552deec9e
Matt Rich <metropolis_4@hotmail.com>
2015-01-29 10:57:21 -0700

    Cleaned up .js

--- main.js
+++ main.js
@@ -1,39 +1,39 @@
 $(document).on('ready', function() {
 
-    // COMPONENTS
+// INITIAL COMPONENTS
 
-	  var $outer = $('<div>')
-  		.addClass('outer-clock');
+    var $outer = $('<div>')
+		.addClass('outer-clock');
 
-  	var $inner = $('<div>')
-  		.addClass('inner-clock');
+	var $inner = $('<div>')
+		.addClass('inner-clock');
 
-  	var $pmDot = $('<div>')
-  		.addClass('dots pmDot hiding');
+	var $pmDot = $('<div>')
+		.addClass('dots pmDot hiding');
 
-  	var $pm = $('<p>')
-  		.text('PM')
-  		.append($pmDot)
-   		.addClass('pm');
+	var $pm = $('<p>')
+		.text('PM')
+		.append($pmDot)
+ 		.addClass('pm');
 
-   	var $autoDot = $('<div>')
- 		  .addClass('dots autoDot');
+ 	var $autoDot = $('<div>')
+		.addClass('dots autoDot');
 
-  	var $auto = $('<p>')
-  		.text('AUTO')
-  		.append($autoDot);
+	var $auto = $('<p>')
+		.text('AUTO')
+		.append($autoDot);
 
-  	var $indicators = $('<div>')
-  		.append($pm).append($auto)
-  		.addClass('indicators');
+	var $indicators = $('<div>')
+		.append($pm).append($auto)
+		.addClass('indicators');
 
-  	var $clockScreen = $('<div>')
-  		.addClass('screen');
+	var $clockScreen = $('<div>')
+		.addClass('screen');
 
-    // AUTO BUTTON
+// AUTO ON/OFF BUTTON
 
     var $button = $('<div>')
-      .addClass('button');
+        .addClass('button');
 
     $(document).on('click', '.button', function(e){
         e.stopPropagation();
@@ -41,39 +41,43 @@ $(document).on('ready', function() {
         $autoDot.toggleClass('hiding');
         $button.animate({height: '20px', top: '-20px'}, 100);
     });
-  		
-    // AM/FM BAND ROWS
+		
+// AM/FM BAND ROWS
 
-  	var amLabels = ['53', '60', '70', '90', '110', '140', '170'];
-  	var fmLabels = ['88', '92', '96', '102', '106', '108'];
+	var amLabels = ['53', '60', '70', '90', '110', '140', '170'];
+	var fmLabels = ['88', '92', '96', '102', '106', '108'];
 
-  	var labelMaker = function(labelArray) {
-  		return labelArray.map(function(label) {
-  			return $("<span class='freqs'> " + label + " </span>");
-  		});
-  	};
+	var labelMaker = function(labelArray) {
+		return labelArray.map(function(label) {
+			return $("<span class='freqs'> " + label + " </span>");
+		});
+	};
 
-  	var $amFreq = labelMaker(amLabels);
-  	var $fmFreq = labelMaker(fmLabels);
+	var $amFreq = labelMaker(amLabels);
+	var $fmFreq = labelMaker(fmLabels);
 
     var makeRows = function(labels, band) {
-      return $('<div>')
-        .addClass('radioRows')
-        .append("<span class='band " + band + "Ind'>" + band + "</span>")
-        .append(labels);
+        return $('<div>')
+            .addClass('radioRows')
+            .append("<span class='band " + band + "Ind'>" + band + "</span>")
+            .append(labels);
     };
 
-    var $amRow = makeRows($amFreq, 'AM').append("<span><span class='little'>X10</span> Khz</span>");
-    var $fmRow = makeRows($fmFreq, 'FM').append("<span class='Hz''>Mhz</span>");
+    var $amRow = makeRows($amFreq, 'AM')
+        .append("<span><span class='little'>X10</span> Khz</span>");
 
-    // SLIDER STATION SELECTOR
+    var $fmRow = makeRows($fmFreq, 'FM')
+        .append("<span class='Hz''>Mhz</span>");
+
+// SLIDER STATION SELECTOR
 
     var $slider = $("<input type='range'>");
+
     $(document).on('click', "input[type='range']", function(e){
-      e.stopPropagation();
+        e.stopPropagation();
     });
 
-    // AM/FM SELECTOR
+// AM/FM SELECTOR
 
     var $amFMSelector = $('<div>')
         .addClass('amFMSelector');
@@ -83,57 +87,57 @@ $(document).on('ready', function() {
 
     $amRow.addClass('selected');
     $amFMSelector.click(function() {
-      $amFMSelector.toggleClass('amFMSelector1');
-      $amRow.toggleClass('selected');
-      $fmRow.toggleClass('selected');
+        $amFMSelector.toggleClass('amFMSelector1');
+        $amRow.toggleClass('selected');
+        $fmRow.toggleClass('selected');
     });
 
-  	// CLOCK FUNCTIONS
+// CLOCK FUNCTIONS
 
     var addZero = function(time) {
-      if(time < 10) {time = "0" + time}
-      return time;
+        if(time < 10) {time = "0" + time}
+        return time;
     };
 
     var amPM = function(am, pm) {
-      pm = am;
-      if(am >= 12) {pm = am - 12}
-      if(pm === 0) {pm = 12}
-      return pm;
+        pm = am;
+        if(am >= 12) {pm = am - 12}
+        if(pm === 0) {pm = 12}
+        return pm;
     };
 
     var addPM = function(hr, name) {
-      if(hr >= 12) {name.removeClass('hiding')}
-      if(hr < 12) {name.addClass('hiding')}
+        if(hr >= 12) {name.removeClass('hiding')}
+        if(hr < 12) {name.addClass('hiding')}
     };
 
     var $hours = $("<span>");
     var $minutes = $("<span class='mins'>");
     var $theTime = $('<div>')
-      .addClass('mainTime');
+        .addClass('mainTime');
 
-    // COLON DEFINITIONS
+// COLON DEFINITIONS
 
     var $colon = $("<p class='colon'></p><p class='colon colon2'></p>");
 
     setInterval(function() {
-      $colon.toggleClass('hiding');
+        $colon.toggleClass('hiding');
     }, 1000);
 
-    // 24HR TIME
-    
+// 24HR TIME
+  
     var $24hr = $('<div>')
-      .text('STD     24hr')
-      .addClass('Letters');
+        .text('STD     24hr')
+        .addClass('Letters');
     var $24hrButton = $('<div>')
-      .addClass('Button');
+        .addClass('Button');
 
     $24hrButton.click(function(e) {
-      $24hrButton.toggleClass('moveButton');
-      e.stopPropagation();
+        $24hrButton.toggleClass('moveButton');
+        e.stopPropagation();
     });
 
-    // TICK FUNCTION
+// TICK FUNCTION
 
     var tick = function(){
 
@@ -142,25 +146,27 @@ $(document).on('ready', function() {
         var hr = now.getHours();
         var hrPM = hr;
 
-        
+      
         if($24hrButton.hasClass('moveButton')) {
-          hrPM = amPM(hr);
+            hrPM = amPM(hr);
         }
         else {
-          hrPM = hrPM;
+            hrPM = hrPM;
         }
 
         hrPM = addZero(hrPM);
 
-        // PM dot
+    // PM dot
 
         addPM(hr, $pmDot);
 
-        // TOTAL TIME
+    // TOTAL TIME
 
         $hours.text(hrPM);
         $minutes.text(min);
-        $theTime.append($hours).append($colon).append($minutes);
+        $theTime.append($hours)
+            .append($colon)
+            .append($minutes);
         $clockScreen.html($theTime);
 
     };
@@ -168,18 +174,20 @@ $(document).on('ready', function() {
     tick();
     setInterval(tick, 100);
 
-  	// PUTTING IT TOGETHER
-  	
-  	$('.clock').append($outer).append($inner);
+// PUTTING IT TOGETHER
+	
+	$('.clock').append($outer).append($inner);
+
     $outer.append($button)
-      .append($amFMSelector)
-      .append($24hr)
-      .append($amFMLetters)
-      .append($24hrButton);
-  	$inner.append($indicators)
-  	  .append($clockScreen)
-  	  .append($amRow)
-      .append($fmRow)
-      .append($slider);
+        .append($amFMSelector)
+        .append($24hr)
+        .append($amFMLetters)
+        .append($24hrButton);
+        
+	$inner.append($indicators)
+	    .append($clockScreen)
+	    .append($amRow)
+        .append($fmRow)
+        .append($slider);
  	
 });
